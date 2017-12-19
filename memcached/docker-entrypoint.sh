ed5651c08b16589908a1e7b6bf2f988e949b4f3c
gdyang <gd.shunhua@qq.com>
2017-12-19 23:16:08 +0800

    memcached

--- memcached/docker-entrypoint.sh
+++ memcached/docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+USER=${MEMCACHED_USERNAME:-memcached}
+PASS=${MEMCACHED_PASSWORD:-$MEMCACHED_PWD}
+
+echo "Creating user: \"$USER\"..."
+echo  /usr/lib/sasl2/memcached.conf
+echo $PASS | saslpasswd2 -a memcached -c $USER -p
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
 	set -- memcached "$@"
