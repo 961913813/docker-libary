c43dd9680630dddca16be45b59e6e6f7e3b94f26
gdyang <gd.shunhua@qq.com>
2017-12-30 22:34:13 +0800

    sasl2

--- memcached/scripts/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-USER=${MEMCACHED_USERNAME:-memcached}
-PASS=${MEMCACHED_PASSWORD:-$(pwgen -s -1 16)}
-
-# Create User
-echo "Creating user: \"$USER\"..."
-echo  /usr/lib/sasl2/memcached.conf
-echo $PASS | saslpasswd2 -a memcached -c $USER -p
