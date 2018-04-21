e334ce2eccd213fd5f54773de428116fa853cde4
gdyang <gd.shunhua@qq.com>
2018-04-21 18:12:32 +0800

    del redis

--- memcached/alpine/docker-entrypoint.sh
+++ memcached/alpine/docker-entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 USER=${MEMCACHED_USERNAME:-memcached}
 PASS=${MEMCACHED_PASSWORD:-$MEMCACHED_PWD}
 
-echo  mech_list: plain >/usr/lib/sasl2/memcached.conf
-chown root:root /etc/sasldb2
-/etc/init.d/saslauthd restart
-echo $PASS | saslpasswd2 -a memcached -c $USER -p
+# echo  mech_list: plain >/usr/lib/sasl2/memcached.conf
+# # chown root:root /etc/sasldb2 
+# /usr/sbin/saslauthd restart
+# echo $PASS | saslpasswd2 -a memcached -c $USER -p
 
 memcached -u root -S -l 0.0.0.0 $@
